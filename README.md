1e677112b9b86b261da29daafb278d849f138527
Matthew_QG <qgan@email.arizona.edu>
2022-07-18 17:30:13 -0400

    Add project 3

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 | ------------- | ------------- |
 | 1 | [Expanding Cards](./src/components/01-expanding-cards/) |
 | 2 | [Progress Steps](./src/components/02-progress-step/)  |
+| 2 | [Rotation navigation animation](./src/components/03-rotation-navigation-animation/)  |
--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'App.css';
+import RotationNavAnimation from './components/03-rotation-navigation-animation/rotationNavAnimation';
 // import ExpandingCards from 'components/01-expanding-cards/expandingCards';
-import ProgressStep from 'components/02-progress-step/progressStep';
+// import ProgressStep from 'components/02-progress-step/progressStep';
 
 function App() {
   return (
     // <ExpandingCards />
-    <ProgressStep />
+    // <ProgressStep />
+    <RotationNavAnimation />
   );
 }
 
